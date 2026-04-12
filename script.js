391bc88bcf724da60edb418cb5617a6997646a2b
saumya <saumya@saumyas-MacBook-Pro-5.local>
2026-04-12 22:30:02 +0530

    Initial setup with Pokemon Explorer and API integration

--- script.js
+++ script.js
@@ -1,30 +1,97 @@
+let allPokemon = []
+
+// List of names (you can expand later)
+let pokemonNames = [
+    "pikachu","charizard","bulbasaur","squirtle",
+    "eevee","snorlax","mew","dragonite",
+    "gengar","machamp"
+]
+
+// Fetch using {name} API
 function getPokemon(){
 
     let loading = document.getElementById("loading")
     let container = document.getElementById("pokemon")
 
     loading.innerText = "Loading..."
+    container.innerHTML = ""
 
-    fetch("https://pokeapi.co/api/v2/pokemon/{name}")
-
-    .then(function(response){
-        return response.json()
+    let promises = pokemonNames.map(function(name){
+        return fetch("https://pokeapi.co/api/v2/pokemon/" + name)
+        .then(res => res.json())
     })
 
+    Promise.all(promises)
     .then(function(data){
 
         loading.innerText = ""
+        allPokemon = data
 
-        container.innerHTML =
-        "<h2>" + data.name + "</h2>" +
-        "<img src='" + data.sprites.front_default + "'>" +
-        "<p>Height: " + data.height + "</p>" +
-        "<p>Weight: " + data.weight + "</p>"
-
+        displayPokemon(allPokemon)
     })
-
     .catch(function(){
         loading.innerText = "Error loading data"
     })
+}
 
+
+// Display
+function displayPokemon(data){
+
+    let container = document.getElementById("pokemon")
+    container.innerHTML = ""
+
+    data.forEach(function(pokemon){
+
+        let attack = pokemon.stats[1].base_stat
+        let hp = pokemon.stats[0].base_stat
+
+        container.innerHTML +=
+        "<div class='card'>" +
+        "<h3>" + pokemon.name + "</h3>" +
+        "<img src='" + pokemon.sprites.front_default + "'>" +
+        "<p>Attack: " + attack + "</p>" +
+        "<p>HP: " + hp + "</p>" +
+        "</div>"
+    })
+}
+
+
+// 🔍 Search (HOF: filter)
+document.getElementById("search").addEventListener("input", function(){
+
+    let value = this.value.toLowerCase()
+
+    let filtered = allPokemon.filter(function(pokemon){
+        return pokemon.name.includes(value)
+    })
+
+    displayPokemon(filtered)
+})
+
+
+// 📊 Sort (HOF: sort)
+document.getElementById("sort").addEventListener("change", function(){
+
+    let value = this.value
+
+    let sorted = [...allPokemon]
+
+    if(value === "attack"){
+        sorted.sort(function(a,b){
+            return b.stats[1].base_stat - a.stats[1].base_stat
+        })
+    }
+
+    if(value === "hp"){
+        sorted.sort(function(a,b){
+            return b.stats[0].base_stat - a.stats[0].base_stat
+        })
+    }
+
+    displayPokemon(sorted)
+})
+
+function toggleDarkMode(){
+    document.body.classList.toggle("dark")
 }
